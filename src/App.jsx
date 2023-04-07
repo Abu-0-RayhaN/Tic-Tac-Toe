b828ceb5d79abac10dc201015ae39f9b363fe3a5
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-08 02:09:09 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import Square from "./components/Square";
+function App() {
+  return (
+    <div>
+      <Square />
+    </div>
+  );
+}
+
+export default App;
