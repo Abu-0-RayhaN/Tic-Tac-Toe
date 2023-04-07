cf6a1adc47b176d8e7f3317583719b12b9f1e55f
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-04-08 03:18:29 +0600

    2nd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import Square from "./components/Square";
+import Board from "./components/Board";
 function App() {
-  return (
-    <div>
-      <Square />
-    </div>
-  );
+  return <Board />;
 }
 
 export default App;
